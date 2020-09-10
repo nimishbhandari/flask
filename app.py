541bf967a927a7a8931cacac5a2dc15a4f96d15c
Nimish <nimishbhandari727521@gmail.com>
2020-09-10 12:38:59 +0530

    1st lecture done

--- app.py
+++ app.py
@@ -21,5 +21,10 @@ def articles():
     return render_template('articles.html', articles=Articles)
 
 
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5050, debug=True)
